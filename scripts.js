00796d5074614982bbac8f3c72b2944378206125
Alessandro <alessandrocosta103@gmail.com>
2022-06-23 16:26:37 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// constante que se refere a API
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/'
+
+// função criada para pegar as informações da API
+const getCats = async () => {
+    try{
+        const data = await fetch(BASE_URL)
+        const json = await data.json()
+        return json.webpurl;
+    }catch (e) {
+        console.log(e.message)
+    }
+}
+
+// constante que carregar a imagem
+    const loading = async () => {
+    const catImg = document.getElementById('cat')
+    catImg.src = await getCats();
+}
+
+loading()
+
+const catBtn = document.getElementById('change-cat')
+catBtn.addEventListener('click', loading);
+
