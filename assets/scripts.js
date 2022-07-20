ebbf5316adfa25190aa931ccf018286eef31671b
Alessandro <alessandrocosta103@gmail.com>
2022-07-20 17:11:23 -0300

    altera api

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,25 +1,25 @@
 // constante que se refere a API
-const BASE_URL = 'https://thatcopy.pw/catapi/rest/'
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const catBtn = document.getElementById('change-cat');
 
 // função criada para pegar as informações da API
 const getCats = async () => {
     try{
-        const data = await fetch(BASE_URL)
-        const json = await data.json()
+        const data = await fetch(BASE_URL);
+        const json = await data.json();
         return json.webpurl;
     }catch (e) {
         console.log(e.message)
     }
-}
+};
 
 // constante que carregar a imagem
     const loading = async () => {
-    const catImg = document.getElementById('cat')
+    const catImg = document.getElementById('cat');
     catImg.src = await getCats();
-}
+};
 
-loading()
 
-const catBtn = document.getElementById('change-cat')
 catBtn.addEventListener('click', loading);
 
+loading();
